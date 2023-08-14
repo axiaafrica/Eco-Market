2aa567d57490644f290e4734a324792979d97afe
Gift Chika <giftchika222@gmail.com>
2023-08-14 06:27:27 -0600

    update

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../assets/style.css'
+import Lastpic from '../assets/Gift-images/last-pic.svg'
 
 const  ProductPage = () => {
   return (
@@ -7,18 +8,18 @@ const  ProductPage = () => {
       <div class="pt-10 relative ">
         
         <div class=" absolute flex flex-row  items-center  gap-[950px] pl-[50px] top-[100px]">
-            <div class="text-[40px] font-[400] text-white"> <span class="text-[60px] font-[700]">5% OFF</span></span> <br><span>ALL MEN'S</span> <br>  SUNGLASS</div>
+            <div class="text-[40px] font-[400] text-white"> <span class="text-[60px] font-[700]">5% OFF</span> <br/><span>ALL MEN'S</span> <br/>  SUNGLASS</div>
             <div class="bg-[#007D6E] text-white inline hover:opacity-60 text-[20px] font-[600] px-14 py-3 rounded-md">Shop Now</div>
         </div>
             
     <div>
-        <img class="w-full" src="/img/last-pic.svg" alt="">
+        <img class="w-full" src= {Lastpic} alt=""/>
     </div>
    
 </div>
 <div class="flex flex-row justify-center gap-[20px] px-[125px] items-end pt-[80px]">
     <div class="left-side ">
-        <img src="/img/Frame 259-gc.png" alt="fashion man standing">
+        <img src="/img/Frame 259-gc.png" alt="fashion man standing"/>
     </div>
     <div class="right-side flex flex-col gap-2">
         <div class="font-[700] text-[43px]">Blue Muscle Fit Long Sleeve Shirt</div>
@@ -47,19 +48,19 @@ const  ProductPage = () => {
     <div class=" flex flex-row items-center justify-center text-[40px] font-[700] pt-[80px]">Styling Ideas</div>
     <div class="flex flex-row justify-between px-[125px] items-end pt-[20px]">
         <div class="flex flex-col gap-3 pb-5">
-            <div><img class="w-[384px]" src="/img/Frame 284-gc.png" alt=""></div>
+            <div><img class="w-[384px]" src="/img/Frame 284-gc.png" alt=""/></div>
             <div class=" text-[20px]">Rosie Sun Shoe</div>
             <div class=" text-[20px] font-[600]">N75,500</div>
             <div class=" text-[20px]  text-[#007D6E]">Sold by <span class="underline">Ola</span></div>
         </div>
         <div class="flex flex-col gap-3 pb-5">
-            <div><img class="w-[384px] h-[304px]" src="/img/watch-gc.png" alt=""></div>
+            <div><img class="w-[384px] h-[304px]" src="/img/watch-gc.png" alt=""/></div>
             <div class=" text-[20px]">John Torcasio Watch</div>
             <div class=" text-[20px] font-[600]">N175,500</div>
             <div class=" text-[20px]  text-[#007D6E]">Sold by <span class="underline">Ola</span></div>
         </div>
         <div class="flex flex-col gap-3 pb-5">
-            <div><img class="w-[384px]" src="/img/Frame 286-gc.png" alt=""></div>
+            <div><img class="w-[384px]" src="/img/Frame 286-gc.png" alt=""/></div>
             <div class=" text-[20px]">Giorgio Trovato Sunglasses</div>
             <div class=" text-[20px] font-[600]">N48,000</div>
             <div class=" text-[20px]  text-[#007D6E]">Sold by <span class="underline">Maddisson</span></div>
